4cdade3e049df2a0824edcae4614d624041ac3c5
Huiying Cao <huiyingcao2@gmail.com>
2025-02-25 16:49:59 -0800

    project1

--- js/app.js
+++ js/app.js
@@ -3,18 +3,24 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+//let board;
+//let winner;
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+//const message = document.querySelector('#message');
+//const squares = document.querySelector('.square');
 
 
 /*-------------------------------- Functions --------------------------------*/
-
+//clear button #reset-game
+//flipping an element
+//if element is flipped, can't flip it again
+//message: win or lose
+//music plays, if win music 1, if lose, music 2
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+//message = 
 
