a1e288afc437d484c724f1d1a6e123ca3db83752
Huiying Cao <huiyingcao2@gmail.com>
2025-02-25 21:39:16 -0800

    project 1

--- js/app.js
+++ js/app.js
@@ -1,26 +1,52 @@
 /*-------------------------------- Constants --------------------------------*/
-
+let correctMatches = 0;
+let totalTries = 0;
+let gameOver = false;
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 //let board;
 //let winner;
+//write a for loop i <= 29, grab each element by ID and assign them a src
+//for (let i =0; i <=29; i++) {
+let img;
+img.style.display = 'none';
+//gameBoard.append(img) does this have dot notation?
+.sqr.addEventListener('click', ())=>
+
+}
+//flip-toggle between visible or no
+//when game initializes, all cards are flipped away
+//function to test matches (equal image src)
+//additional logic to ensure only 2 square are flipped at any time
+//game end -accuracy too low or all squares flipped
+//work on shuffling images...look up shuffling Fisher Yates shuffle
 
 
 /*------------------------ Cached Element References ------------------------*/
 //const message = document.querySelector('#message');
+//const gameBoard = document.querySelector('.gameBoard');
 //const squares = document.querySelector('.square');
+//const resetButton = document.querySelector('#reset-game')
 
 
 /*-------------------------------- Functions --------------------------------*/
 //clear button #reset-game
+//function reset() {
+//}
 //flipping an element
 //if element is flipped, can't flip it again
 //message: win or lose
+//function accuracy ()
 //music plays, if win music 1, if lose, music 2
+
+//function () {
+}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-//message = 
+// //resetButton.addEventListener('click', () => {
+// 	reset();
+// 	});
 
