b19bfd3ab8be44411019843ed01ba8f17fedd360
Huiying Cao <huiyingcao2@gmail.com>
2025-02-28 08:43:02 -0800

    project 1 update

--- README.md
+++ README.md
@@ -5,13 +5,11 @@
 
 *The game features a 5 by 6 grid and its aim is for the user to find as many matching images in the fewest number of tries. The game is over when player accuracy dips below 55% or if all the cards have been matched.
 
-*The game will play a sound when the player wins and a different one if there have been too many tries.
+*The game will play a sound when the player wins. 
 
 *The game is built using HTML, CSS, and Javascript. I selected it as a simpler project because I see some similarities between it and the tic-tac-toe exercise from earlier.
 
-Attributions: information about the Fisher-Yates shuffle was provided here:
+Attributions: The function for the  Fisher-Yates shuffle was provided here:
 [GeeksforGeeks](https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/)
 
 
-
-screenshot not working, tested in preview
--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
-/*-------------------------------- Constants --------------------------------*/
-let correctMatches = 0;
-let totalTries = 0;
-let gameOver = false;
-let firstGuess = null;
-let canGuess = true;
-let flippedCards = 0;
+//create an aray of objects
+let cards = [
+  {value: '0', image: 'images/image0.jpg', matched: false},
+  {value: '1', image: 'images/image1.jpg', matched: false},
+  {value: '2', image: 'images/image2.jpg', matched: false},
+  {value: '3', image: 'images/image3.jpg', matched: false},
+  {value: '4', image: 'images/image4.jpg', matched: false},
+  {value: '5', image: 'images/image5.jpg', matched: false},
+  {value: '6', image: 'images/image6.jpg', matched: false},
+  {value: '7', image: 'images/image7.jpg', matched: false},
+  {value: '8', image: 'images/image8.jpg', matched: false},
+  {value: '9', image: 'images/image9.jpg', matched: false},
+  {value: '10', image: 'images/image10.jpg', matched: false},
+  {value: '11', image: 'images/image11.jpg', matched: false},
+  {value: '12', image: 'images/image12.jpg', matched: false},
+  {value: '13', image: 'images/image13.jpg', matched: false},
+  {value: '14', image: 'images/image14.jpg', matched: false},
+  {value: '0', image: 'images/image0.jpg', matched: false},
+  {value: '1', image: 'images/image1.jpg', matched: false},
+  {value: '2', image: 'images/image2.jpg' ,matched: false},
+  {value: '3', image: 'images/image3.jpg', matched: false},
+  {value: '4', image: 'images/image4.jpg', matched: false},
+  {value: '5', image: 'images/image5.jpg', matched: false},
+  {value: '6', image: 'images/image6.jpg', matched: false},
+  {value: '7', image: 'images/image7.jpg', matched: false},
+  {value: '8', image: 'images/image8.jpg', matched: false},
+  {value: '9', image: 'images/image9.jpg', matched: false},
+  {value: '10', image: 'images/image10.jpg', matched: false},
+  {value: '11', image: 'images/image11.jpg', matched: false},
+  {value: '12', image: 'images/image12.jpg', matched: false},
+  {value: '13', image: 'images/image13.jpg', matched: false},
+  {value: '14', image: 'images/image14.jpg', matched: false},
+]
 
-
-/*---------------------------- Variables (state) ----------------------------*/
-
-function fisherYatesShuffle(array) {
+function FyShuffle(array) {
   // Create a copy of the original array to avoid modifying it
   const shuffled = [...array];
   
@@ -22,110 +45,91 @@ function fisherYatesShuffle(array) {
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
   return shuffled;
+}
 
-}let squares = [
-  {value: '0', image: 'images/image0.jpg'},
-  {value: '1', image: 'images/image1.jpg'},
-  {value: '2', image: 'images/image0.jpg'},
-  {value: '3', image: 'images/image0.jpg'},
-  {value: '4', image: 'images/image0.jpg'},
-  {value: '5', image: 'images/image0.jpg'},
-  {value: '6', image: 'images/image0.jpg'},
-  {value: '7', image: 'images/image0.jpg'},
-  {value: '8', image: 'images/image0.jpg'},
-  {value: '9', image: 'images/image0.jpg'},
-  {value: '10', image: 'images/image0.jpg'},
-  {value: '11', image: 'images/image0.jpg'},
-  {value: '12', image: 'images/image0.jpg'},
-  {value: '13', image: 'images/image0.jpg'},
-  {value: '14', image: 'images/image0.jpg'},
-  {value: '0', image: 'images/image0.jpg'},
-  {value: '1', image: 'images/image1.jpg'},
-  {value: '2', image: 'images/image0.jpg'},
-  {value: '3', image: 'images/image0.jpg'},
-  {value: '4', image: 'images/image0.jpg'},
-  {value: '5', image: 'images/image0.jpg'},
-  {value: '6', image: 'images/image0.jpg'},
-  {value: '7', image: 'images/image0.jpg'},
-  {value: '8', image: 'images/image0.jpg'},
-  {value: '9', image: 'images/image0.jpg'},
-  {value: '10', image: 'images/image0.jpg'},
-  {value: '11', image: 'images/image0.jpg'},
-  {value: '12', image: 'images/image0.jpg'},
-  {value: '13', image: 'images/image0.jpg'},
-  {value: '14', image: 'images/image0.jpg'},
-]
+let cardEls =document.querySelectorAll('.faceDownCard');
+let correctMatches = 0;
+let totalTries = 0;
+let gameOver = false;
+let firstGuess = null;
+let canGuess = true;
+let guessCount = 0;
+let flippedCards = 0;
 
 
-//write a for loop i <= 29, grab each element by ID and assign them a src
-//for (let i =0; i <=29; i++) {
+FyShuffle(cards);
 
-//img.style.display = 'none';
-//gameBoard.append(img) does this have dot notation?
+//console.log which index clicked
+cardEls.forEach(function(el, index) {
+  el.addEventListener('click', function() {
+    if (index === firstGuess || cards[index].matched === true || !canGuess) {
+      console.log('same card clicked twice');
+      return
+    }
+    let clickedCard = cards[index]
+    el.setAttribute('src', clickedCard.image);
 
+    if (firstGuess === null) {
+        firstGuess= index;
+        guessCount++
+        totalTries++;
+    } else {
+      if (cards[firstGuess].value === cards[index].value) {
+        console.log('match');
+        cards[firstGuess].matched = true;
+        cards[index].matched === true;
+        firstGuess = null;  //match
+        flippedCards+=2;
+        correctMatches++
+        updateMessage();
+        //check for win, reset
+        if(flippedCards === cards.length) {
+          resetGame();
+        }
+      } else {
+        canGuess = false;
+        console.log('no match'); //no match
+        updateMessage();
 
+        setTimeout(() => {
+          cardEls[firstGuess].setAttribute('src', "images/geode closed.jpg");
+          cardEls[index].setAttribute('src', "images/geode closed.jpg");
+          firstGuess = null;
+          canGuess = true;
+        } , 1500)
+        }
+        
+      }
+    });
+});
 
-//when game initializes, all cards are flipped away
-//function to test matches (equal image src)
-//additional logic to ensure only 2 square are flipped at any time
-//game end -accuracy too low or all squares flipped
-//work on shuffling images...look up shuffling Fisher Yates shuffle
+function resetGame() {
+  canGuess = false;
+  setTimeout(() => {
+  }, 2000);
 
+  firstGuess = null;
+  canGuess = true;
+  flippedCards = 0;
+  guessCount = 0;
 
+  cards.forEach((card, index) => {
+    card.matched = false;
+  });
 
+  FyShuffle(cards); // Shuffle the cards array
 
-/*------------------------ Cached Element References ------------------------*/
-//const message = document.querySelector('#message');
-const flipCard = document.querySelectorAll('.flip-card');
-const gameBoard = document.getElementsByClassName('.flip-inner');
-const resetButton = document.querySelector('#reset-game');
-const message = document.getElementById('message');
+  cardEls.forEach(function(el, index) {
+    el.setAttribute('src', "images/geode closed.jpg"); //reset to closed image.
+  });
 
-
-/*-------------------------------- Functions --------------------------------*/
-//clear button #reset-game
-//function reset() {
-
-//
-//
-// flipping an element
-//if element is flipped, can't flip it again
-//remove event listener for clicked squares
-
-for (let i =0; i <30; i++) {
-  const img = document.createElement('img');
-  img.src ='./images/geode closed.jpg';
-  square.appendChild(img);
+  updateMessage();
 }
 
 
-//
-
-//Does this have all the same conditions as reset?
-function initializeGame () {
-accuracyRate(); 
-fisherYatesShuffle(squares);
-squares.imagesource = "./images/geode closed.jpg";
-}
-
-function flip(cardContainer) {
-  cardContainer.style.transform="rotatey(180deg)";
-  cardContainer.style.transitionDuration="1.5s";
-}
-
-
-// if a square is clicked, set its evt Listener to -off- for now
-// let the first clicked be square 1
-// let the second one clicked be square 2
-// flip 2 squares
-// if square1.imagesource === square2.imagesource
-// flippedCards+= 2;
-// return "it's a match!";
-
-// disable evt listeners on those two squares
-// else 
-// return "Nope"; turn evt listeners back on for the 2 clicked squares
-// updateMessage();
+document.querySelector("#reset").addEventListener('click', function() {
+  resetGame();
+});
 
 //check accuracy
 function accuracyRate () {
@@ -141,67 +145,15 @@ function updateMessage() {
   let accuracy = accuracyRate();
   const message = document.getElementById("message");
   if (message) {
-    message.textContent ="You are at ${accuracy.toFixed(2)} percent"
+    message.textContent =`You are at ${accuracy.toFixed(2)} percent`;
   }
 
 }
 
-//
-function endGame() {
-  accuracyRate();
-  if(flippedCards === cards.length || accuracyRate <= 55/100) {
+function checkGameOver() {
+  if (guessCount >= 10 && accuracyRate() <= 50) {
     gameOver = true;
-    message.textContent="Game Over";
-    square.removeEventListner('click');
+    updateMessage(); //update to game over message.
+    canGuess = false; //prevent further clicks.
   }
 }
-  function resetGame() {
-    canGuess = false;
-    firstGuess = null;
-    canGuess = true;
-    cardEls.forEach(else, value) 
-    fisherYatesShuffle(squares);
-  }
- 
-function gamePlay() {
-  initializeGame();
-  updateMessage();
-  endGame();
-
-}
-  //music plays, if win music 1, if lose, music 2
-//
-
-
-//function playWinMusic() {}
-
-//grab all elements with class .sqr  assign them an image
-//flip-toggle between visible or no
-// squares.forEach((square, index) => {
-// 	square.style.backgroundImage = `url('../images/image${index}.jpg')`;
-// 	});
-// 	
-
-
-	
-//assigning images to squares
-const halfLength =squares.length/2;
-
-for (let i =0; i < halfLength; i ++) {
-	squares[i].style.backgroundImage = `url('../images/image'${i}'.jpg')`;
-	squares[i+15].style.backgroundImage = `url('../images/image'${i}'.jpg')`;
-/////////////`url('${imagePath}')`;
-}
-
-/*----------------------------- Event Listeners -----------------------------*/
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    img.style.display = img.style.display === 'none'
-      ? 'block' : 'none';
-      console.log('clicked');
-  });
-});
-
-resetButton.addEventListener('click', () => {
-  reset();
-});
